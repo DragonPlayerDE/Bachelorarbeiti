0ee96be954e8cc52e5a7a66a414cc1cca25df97f
Tom <s6krautm@uni-bonn.de>
2026-01-20 15:31:08 +0100

    Relax zur Funktion gemacht

--- test.py
+++ test.py
@@ -4,42 +4,50 @@ from itertools import combinations
 items = []
 
 for i in range(20):
-    weight = random.random()         # zufälliges Gewicht
-    value = random.random()          # zufälliger Wert zwischen 0 und 1
-    stonks = value / weight          # Effizienz der Items
-    items.append((weight, value, stonks))
+    weight = random.random()  # zufälliges Gewicht
+    value = random.random()  # zufälliger Wert zwischen 0 und 1
+    stonks = value / weight  # Effizienz der Items
+    number = i
+    items.append((weight, value, stonks, number))
+
 
 # Ausgabe der ersten 10 zur Kontrolle
-#for item in items[:10]:
+# for item in items[:10]:
 #    print(item)
 
-def stonks_sort(e):      # Hilfsfunktion für Sortieren
-  return e[2]
+def stonks_sort(e):  # Hilfsfunktion für Sortieren
+    return e[2]
 
 
 items.sort(key=stonks_sort, reverse=True)
 
 # Ausgabe der ersten 10 sortierten zur Kontrolle
-#for item in items[:10]:
+# for item in items[:10]:
 #   print(item)
 
 gesamt_volumen = 1
-inhalt = 0
-relaxierte_loesung = 0
-steigung_core = 0
-core_item = -1
 
-#Relaxierte Lösung
-for item in items:
-    core_item +=1
-    if item[0] + inhalt < gesamt_volumen:
-        inhalt += item[0]
-        relaxierte_loesung += item[1]
-    else:
-        relaxierte_loesung += item[1] / (item[0] / (gesamt_volumen - inhalt))
-        inhalt = gesamt_volumen
-        steigung_core = item[2]
-        break
+
+# Relaxierte Lösung
+def relax(items):
+    core_item = -1
+    inhalt = 0
+    relaxierte_loesung = 0
+    steigung_core = 0
+    for item in items:
+        core_item += 1
+        if item[0] + inhalt < gesamt_volumen:
+            inhalt += item[0]
+            relaxierte_loesung += item[1]
+        else:
+            relaxierte_loesung += item[1] / (item[0] / (gesamt_volumen - inhalt))
+            inhalt = gesamt_volumen
+            steigung_core = item[2]
+            break
+    return core_item, relaxierte_loesung, steigung_core, core_item
+
+
+core_item, relaxierte_loesung, steigung_core, core_item = relax(items)
 
 print("Das Core Item ist: ", core_item)
 print("Die Relaxierte Lösung ist: ", relaxierte_loesung)
@@ -49,23 +57,26 @@ items_for_core = []
 for item in items:
     weight = item[0]  # Gewichtsübernahem
     value = item[1]  # Wertübernahme
-    stonks = item[2] # Stonkübernahme
-    entfehrnung_core = -1*((steigung_core*weight)-value)
-    items_for_core.append((weight, value, stonks, entfehrnung_core))
+    stonks = item[2]  # Stonkübernahme
+    entfehrnung_core = -1 * ((steigung_core * weight) - value)
+    number = item[3] # Idexübernahme
+    items_for_core.append((weight, value, stonks, entfehrnung_core, number))
 
 
 # Ausgabe der ersten 10 Items für Core zur Kontrolle
-#for item in items_for_core[:10]:
+# for item in items_for_core[:10]:
 #    print(item)
 
-def entfehrnung_sort(e):      # Hilfsfunktion für Sortieren
+def entfehrnung_sort(e):  # Hilfsfunktion für Sortieren
     return abs(e[3])
+
 
 items_for_core.sort(key=entfehrnung_sort)
 
+
 # Ausgabe der ersten 10 Items für Core nach Sortieren zur Kontrolle
-#for item in items_for_core[:10]:
- #   print(item)
+# for item in items_for_core[:10]:
+#   print(item)
 
 def knapsack_bruteforce(items, capacity):
     n = len(items)
@@ -95,27 +106,27 @@ abstand_zu_ray = items_for_core[0][3]
 for item in items_for_core:
     if item[3] > abstand_zu_ray:
         fehler += 1
-        if core_item+1 <= fehler:
+        if core_item + 1 <= fehler:
             print("Ich habe scheisse gebaut")
             print(item[3])
             print(fehler)
         core_volumen -= item[0]
         core_value += item[1]
 
-
 for item in items_for_core:
-    if abs(item[3]) > relaxierte_loesung-(core_value+value_help):
-        print("Die Core Lösung ist: ", core_value+value_help)
+    if abs(item[3]) > relaxierte_loesung - (core_value + value_help):
+        print("Die Core Lösung ist: ", core_value + value_help)
         print(f"Der Core war {len(items_in_core)} Groß: ")
         break
     else:
-        if item[3] > 0: #Wenn Item über Dantzig Ray liegt
+        if item[3] > 0:  # Wenn Item über Dantzig Ray liegt
             core_volumen += item[0]
             core_value -= item[1]
         items_in_core.append(item)
         if len(items_in_core) > 20:
             print("Aua es tut so weh")
         value_help, items_help = knapsack_bruteforce(items_in_core, core_volumen)
+# def branch_bound():
 
 print("Gewählte Items:", items_help)
 

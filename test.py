4f0826e4ef11f7b8c5a4694f8f91f9d7fc5e74dc
Tom <s6krautm@uni-bonn.de>
2026-01-30 20:04:58 +0100

    KP

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ import copy
 import time
 
 items = []
+elemente = 20000
 
-for i in range(1000):
+for i in range(elemente):
     weight = random.random()  # zufälliges Gewicht
     value = random.random()  # zufälliger Wert zwischen 0 und 1
     stonks = value / weight  # Effizienz der Items
@@ -51,7 +52,7 @@ def relax(items, volumen):
 
 core_item, relaxierte_loesung, steigung_core = relax(items, gesamt_volumen)
 
-print("Das Core Item ist: ", core_item)
+#print("Das Core Item ist: ", core_item)
 print("Die Relaxierte Lösung ist: ", relaxierte_loesung)
 
 items_for_core = []
@@ -100,7 +101,7 @@ def knapsack_bruteforce(items, capacity):
 
 def pareto_knapsack(items, max_weight):
     """
-    items: Liste von (gewicht, wert)
+    items: Liste von (gewicht, wert, indexnummer)
     max_weight: Maximales Volumen/Gewicht des Rucksacks
 
     Rückgabe:
@@ -163,7 +164,7 @@ for item in items_for_core:
         core_end_time = time.perf_counter()
         print("Die Core Lösung ist: ", core_value + value_help)
         print("Die Core Laufzeit war: ", core_end_time-core_start_time)
-        #print(f"Der Core war {len(items_in_core)} Groß")
+        print(f"Der Core war {len(items_in_core)} Groß")
         break
     else:
         if item[3] > 0:  # Wenn Item über Dantzig Ray liegt
@@ -176,59 +177,80 @@ for item in items_for_core:
         value_help = pareto_knapsack(items_in_core, core_volumen)
 
 
-bestes_blatt =0
+bestes_blatt= []
+bestes_blatt.append(0)
+#bestes_blatt.append([])
+
 bauminit = []
-bauminit.append(items)
+bauminit.append(0)#Aktuelle Branch Position
 bauminit.append(gesamt_volumen)
 bauminit.append(relaxierte_loesung)#Wert Relaxlösung + feste Objekte
 bauminit.append(0)#Wert von festen Objekten
+bauminit.append([])#Elemente der Lösung
 baum = []
 baum.append(bauminit)
 global bound_time
 bound_time = 0
+global suchen_time
 suchen_time = 0
 branch_time= 0
+#global hinzugefügt
+#hinzugefügt = 0
+#global gelöscht
+#gelöscht = 0
 #print(baum)
 
 def bound(top):
     global bound_time
-    bound_start_time = time.perf_counter()
+    #global gelöscht
+    #bound_start_time = time.perf_counter()
     #print(top)
     full_counter = 0
     #print(max(baum, key=lambda x: x[2])[2])
     for i in baum:
         if i[2] <= top:
             baum.pop(full_counter)
+            #gelöscht +=1
         full_counter += 1
-    bound_time +=(time.perf_counter()-bound_start_time)
+    #bound_time +=(time.perf_counter()-bound_start_time)
 
 
 
 def branch(baumitem, top):
+    #global hinzugefügt
     #print(baumitem)
     #print(baumitem[0])
-    wert_item = baumitem[0][0][1]
-    gewicht_item = baumitem[0][0][0]
-    #TODO index_item = baumitem[0][0][3]
+    global suchen_time
+    wert_item = items[baumitem[0]][1]
+    gewicht_item = items[baumitem[0]][0]
+    #index_item = items[baumitem[0]][3]
     #Bestes Item wird auf 0 gesetzt
-    baumitem[0].pop(0)
-    baumitem[2]=relax(baumitem[0], baumitem[1])[1] + baumitem[3]
+    baumitem[0] += 1
+    baumitem[2]=relax(items[baumitem[0]:elemente], baumitem[1])[1] + baumitem[3]
     #print("2.aufrgu", baumitem)
-    if baumitem[0] != [] and baumitem[2]>top:
+    if baumitem[0] != elemente-1 and baumitem[2]>top[0]:
+        suchen_start_time = time.perf_counter()
         baum.append(copy.deepcopy(baumitem))
+        suchen_time += (time.perf_counter() - suchen_start_time)
+        #hinzugefügt +=1
 
     # Bestes Item wird auf 1 gesetzt
     if baumitem[1]-gewicht_item >= 0:
         baumitem[3] += wert_item
         baumitem[1] -= gewicht_item
-        baumitem[2] = relax(baumitem[0], baumitem[1])[1] + baumitem[3]
+        baumitem[2] = relax(items[baumitem[0]:elemente], baumitem[1])[1] + baumitem[3]
+        #baumitem[4].append(index_item)
         #print("3.aufrgu", baumitem)
-        if baumitem[3] > top:
-            top = baumitem[3]
+        if baumitem[3] > top[0]:
+            top[0] = baumitem[3]
+            #top[1] = baumitem[4]
             #print(top)
-            bound(top)
-        if baumitem[0] != [] and baumitem[2] > top:
+            bound(top[0])
+        if baumitem[0] != elemente-1 and baumitem[2] > top[0]:
+            suchen_start_time = time.perf_counter()
             baum.append(copy.deepcopy(baumitem))
+            suchen_time += (time.perf_counter() - suchen_start_time)
+            #hinzugefügt += 1
 
     return top
     #Ein Element wird Gewählt und auf 1 und 0 gesetzt
@@ -243,40 +265,42 @@ def branch(baumitem, top):
 #print("Gewählte Items:", best_items)
 
 #Nemhauser-Ulmann auf alles
-pareto_start_time = time.perf_counter()
-best_value = pareto_knapsack(items_for_core, gesamt_volumen)
-pareto_end_time = time.perf_counter()
-print("Pareto Lösung:", best_value)
-print("Die Pareto Laufzeit war:", pareto_end_time-pareto_start_time)
+#pareto_start_time = time.perf_counter()
+#best_value = pareto_knapsack(items_for_core, gesamt_volumen)
+#pareto_end_time = time.perf_counter()
+#print("Pareto Lösung:", best_value)
+#print("Die Pareto Laufzeit war:", pareto_end_time-pareto_start_time)
 #TODO print("Gewählte Items:", best_items)
 
 #Branch and Bound auf alles
-#aua=0
+aua=0
 bnb_start_time = time.perf_counter()
 while baum != []:
-    #aua +=1
+    aua +=1
     index, _ = max(enumerate(baum), key=lambda x: x[1][2])
     #print(baum[index][2])
     #print(index)
     #print("Hier Baum Bruder", baum)
     suchen_start_time = time.perf_counter()
     zwischenspeicher = copy.deepcopy(baum[index])
-    print(zwischenspeicher)
+    #print(zwischenspeicher)
     suchen_time += (time.perf_counter() - suchen_start_time)
     baum.pop(index)
-
-    branch_start_time = time.perf_counter()
+    #gelöscht += 1
+    #branch_start_time = time.perf_counter()
     bestes_blatt= branch(zwischenspeicher, bestes_blatt)
-    branch_time += (time.perf_counter() - branch_start_time)
+    #branch_time += (time.perf_counter() - branch_start_time)
     #print(baum)
     #if aua%1000==0:
         #print("Aua es tut so weh:", len(baum))
-        #print(max(baum, key=lambda x: x[2])[2])
-#print("BnB hat so viele Runden gebraucht :" ,aua)
+        #print("Es wurden so viele Knoten hinzugefügt und gelöscht", hinzugefügt, gelöscht)
+        #hinzugefügt= 0
+        #gelöscht = 0
+        #print("Der beste Knoten könnte noch auf so viel kommen :", max(baum, key=lambda x: x[2])[2])
+print("BnB hat so viele Runden gebraucht :" ,aua)
 bnb_end_time = time.perf_counter()
-print("Die Branch_Bound Lösung ist: ", bestes_blatt)
+print("Die Branch_Bound Lösung ist: ", bestes_blatt[0])#, "mit den Elementen:", sorted(bestes_blatt[1]))
 print("Die Branch_Bound Laufzeit war:", bnb_end_time-bnb_start_time)
-print("Bound hat so lange gebraucht:", bound_time)
-print("Das Wählen des nächsten Elements hat so lange gebraucht: ", suchen_time)
-print("Branch hat so lange gebraucht: ", branch_time)
-
+#print("Bound hat so lange gebraucht:", bound_time)
+print("Das kopieren hat so lange gebraucht: ", suchen_time)
+#print("Branch hat so lange gebraucht: ", branch_time)
